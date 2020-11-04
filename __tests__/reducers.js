cbb7dc496f661f5c7f81808aeb79310aebcea6f6
Scott Burman <scottburs@gmail.com>
2020-11-04 14:37:05 -0800

    Tested a bunch

--- __tests__/reducers.js
+++ __tests__/reducers.js
@@ -2,7 +2,7 @@ import {wobbeReducer} from '../client/reducers/wobbeReducer';
 
 
 
-describe('default state', function() {
+describe('wobbeReducerTest', function() {
   let state;
   beforeEach(() => {
    state = {username: '',
@@ -23,5 +23,28 @@ describe('default state', function() {
       expect(wobbeReducer(state, action)).toBe(state);
     });
   });
+
+  describe('SET_USERNAME', () => {
+    const action = {
+      type: 'SET_USERNAME',
+      payload: 'Scott'
+    }
+    it('updates username with action payload', () => {
+      const {username} = wobbeReducer(state, action);
+      expect(username).toBe(action.payload);
+    })
+  
+  it('returns a state object not strictly equal to the original', () => {
+    const{ state } = wobbeReducer(state, action); 
+    expect(wobbeReducer(undefined, {})).not.toStrictEqual(state)
+  });
+
+  it('doesn\'t touch the itemList array', () => {
+    const { list } = wobbeReducer(state, action);
+    expect(list).toEqual([]);
+  });
+
+})
+
 });
  
