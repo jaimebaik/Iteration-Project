f6837b87924255256c2efee79d818f5346917535
Jarryl Oquias <jaroqui17@gmail.com>
2020-11-04 18:22:17 -0800

    Got Enzyme testing up and running

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,5 +1,23 @@
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import { configure, shallow, mount, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import configureMockStore from 'redux-mock-store';
 import App from '../client/components/app';
+
+configure({ adapter: new Adapter() });
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('A suite', function() {
+    it('renders without crashing', () => {
+        const store = mockStore({}); // Instead of {}, you can give your initial store
+        shallow(
+          <Provider store={store}>   // Provides the store to your App
+            <App />
+          </Provider>
+        );
+      });
+});
